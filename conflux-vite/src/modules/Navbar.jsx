4546422b5769257c634ac6485f3c4bee18b0de28
Karalyos Gergo <gergokaralyos@gmail.com>
2023-02-09 18:16:50 +0100

    Installed iconsax-react via npm

--- conflux-vite/src/modules/Navbar.jsx
+++ conflux-vite/src/modules/Navbar.jsx
@@ -5,6 +5,7 @@ function Navbar() {
     return (
       <div className="nav-main">
         <a href='#' className='menu-button'>M</a>
+        
         <a href='#' className='logo-text'>CONFLUX</a>
         <a href='#' className='user-button'>P</a>
       </div>
