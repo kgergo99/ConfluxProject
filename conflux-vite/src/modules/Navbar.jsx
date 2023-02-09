4825291feb29d7a95b489abaa845c9de4b1fe2e1
Karalyos Gergo <gergokaralyos@gmail.com>
2023-02-09 18:00:29 +0100

    Removed Boilerplate, Start of NavBar added

---
+++ conflux-vite/src/modules/Navbar.jsx
@@ -0,0 +1,15 @@
+import './navbar.css'
+
+function Navbar() {
+
+    return (
+      <div className="nav-main">
+        <a href='#' className='menu-button'>M</a>
+        <a href='#' className='logo-text'>CONFLUX</a>
+        <a href='#' className='user-button'>P</a>
+      </div>
+    )
+  }
+  
+  export default Navbar
+  
