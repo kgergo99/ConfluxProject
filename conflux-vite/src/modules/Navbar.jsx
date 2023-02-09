e2d7e77c4e9fb93ddd0a036a48129b775d94f158
Karalyos Gergo <gergokaralyos@gmail.com>
2023-02-09 18:50:03 +0100

    Added icons for navbar items

--- conflux-vite/src/modules/Navbar.jsx
+++ conflux-vite/src/modules/Navbar.jsx
@@ -1,13 +1,13 @@
 import './navbar.css'
+import { HambergerMenu, User } from 'iconsax-react';
 
 function Navbar() {
 
     return (
       <div className="nav-main">
-        <a href='#' className='menu-button'>M</a>
-        
-        <a href='#' className='logo-text'>CONFLUX</a>
-        <a href='#' className='user-button'>P</a>
+        <div className='navbar-item'><HambergerMenu href='#' className='svg-button menu' size="2.5rem" color="black" variant="Outline"/></div>     
+        <a href='#' className='logo-text navbar-item'>CONFLUX</a>
+        <div className='navbar-item'><User href='#' className='svg-button user' size="2.5rem" color="black" variant="Outline"/></div>
       </div>
     )
   }
