50c24af082504b1f2fb2694be23a8b864d0da1b7
Karalyos Gergo <gergokaralyos@gmail.com>
2023-02-15 19:18:13 +0100

    Card component for representing Decks WIP

--- conflux-vite/src/dinamic_modules/DeckListAssembler.jsx
+++ conflux-vite/src/dinamic_modules/DeckListAssembler.jsx
@@ -3,7 +3,7 @@ import DeckCard from '../modules/DeckCard'
 function DeckListAssembler() {
 
     return ( 
-      <DeckCard />
+      <DeckCard name={"Template Name"} image={"https://www.artofmtg.com/wp-content/uploads/2023/01/Infested-Fleshcutter-Phyrexia-All-Will-Be-One-MtG-Art-1536x864.jpg"} deckSize={75} collected={62} colors={['R','G','B']}/>
     )
   }
   
--- conflux-vite/src/modules/DeckCard.jsx
+++ conflux-vite/src/modules/DeckCard.jsx
@@ -1,11 +1,24 @@
 import './modules.css'
+import './deckcardeffects.css'
 
-function DeckCard() {
+function DeckCard({ name, image, deckSize, collected, colors  }) {
 
     return (
-      <div className="div-card">
-        
+      <div className='card-container'>
+        <div className="div-card">
+          <div className='card-content-container'>
+            <h2 className='deck-name'>{name}</h2>
+            <div className='collection-text-div'><h3>{collected} / {deckSize}</h3></div>
+            <div className='collection-bar-div'></div>
+          </div>
+          
+          <div className='deck-image-div'>
+            <img src="https://www.mtgpics.com/pics/art/one/010.jpg"></img>
+          </div>
+          
+        </div>
       </div>
+      
     )
   }
   
