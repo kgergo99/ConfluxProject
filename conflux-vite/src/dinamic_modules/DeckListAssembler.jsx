abf1fe50bc161ef26771515851decf236f0c920b
Karalyos Gergo <gergokaralyos@gmail.com>
2023-02-16 14:53:09 +0100

    Card element for decks complete (sort-of)

--- conflux-vite/src/dinamic_modules/DeckListAssembler.jsx
+++ conflux-vite/src/dinamic_modules/DeckListAssembler.jsx
@@ -2,8 +2,26 @@ import DeckCard from '../modules/DeckCard'
 
 function DeckListAssembler() {
 
+  var stylingObject = {
+    div: {
+      display: "grid",
+      gap: "2rem",
+      gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+    }
+  };
+
     return ( 
-      <DeckCard name={"Template Name"} image={"https://www.artofmtg.com/wp-content/uploads/2023/01/Infested-Fleshcutter-Phyrexia-All-Will-Be-One-MtG-Art-1536x864.jpg"} deckSize={75} collected={62} colors={['R','G','B']}/>
+      <div className='deck-grid' style={stylingObject.div}>
+        <DeckCard name={"Template Name"} image={"https://www.mtgpics.com/pics/art/one/010.jpg"} deckSize={75} collected={60} colors={['R','G','B']}/>
+        <DeckCard name={"Template Name 2"} image={"https://www.mtgpics.com/pics/art/one/232.jpg"} deckSize={75} collected={2} colors={['W','B']}/>
+        <DeckCard name={"Template Name 2"} image={"https://www.mtgpics.com/pics/art/one/232.jpg"} deckSize={75} collected={56} colors={['W','B']}/>
+        <DeckCard name={"Template Name 2"} image={"https://www.mtgpics.com/pics/art/one/232.jpg"} deckSize={75} collected={14} colors={['W','B']}/>
+        <DeckCard name={"Template Name 2"} image={"https://www.mtgpics.com/pics/art/one/232.jpg"} deckSize={75} collected={44} colors={['W','B']}/>
+        <DeckCard name={"Template Name 2"} image={"https://www.mtgpics.com/pics/art/one/232.jpg"} deckSize={75} collected={30} colors={['W','B']}/>
+        <DeckCard name={"Template Name"} image={"https://www.mtgpics.com/pics/art/one/010.jpg"} deckSize={75} collected={75} colors={['R','G','B']}/>
+        <DeckCard name={"Template Name"} image={"https://www.mtgpics.com/pics/art/one/010.jpg"} deckSize={75} collected={74} colors={['R','G','B']}/>
+      </div>
+      
     )
   }
   
--- conflux-vite/src/modules/DeckCard.jsx
+++ conflux-vite/src/modules/DeckCard.jsx
@@ -1,7 +1,26 @@
 import './modules.css'
 import './deckcardeffects.css'
+import React from 'react';
 
 function DeckCard({ name, image, deckSize, collected, colors  }) {
+    var collectedPercent = (collected / (deckSize / 100)) + '%'
+    //document.documentElement.style.setProperty('--collected-percent', collectedPercent);
+
+    const grey =getComputedStyle(document.documentElement).getPropertyValue('--grey');
+    const barHeight =getComputedStyle(document.documentElement).getPropertyValue('--collection-bar-height');
+    const maxWidth =getComputedStyle(document.documentElement).getPropertyValue('--max-width');
+    const borderRad =getComputedStyle(document.documentElement).getPropertyValue('--border-rad');
+    const accentYellow =getComputedStyle(document.documentElement).getPropertyValue('--accent-yellow');
+
+    var stylingObject = {
+      div: {
+        backgroundColor: `${grey}`,
+        height: `${barHeight}`,
+        width: `${maxWidth}`,
+        borderRadius: `0 0 ${borderRad} ${borderRad}`,
+        background: `linear-gradient(90deg, ${accentYellow} ${collectedPercent}, ${grey} ${collectedPercent}`
+      }
+    };
 
     return (
       <div className='card-container'>
@@ -9,11 +28,12 @@ function DeckCard({ name, image, deckSize, collected, colors  }) {
           <div className='card-content-container'>
             <h2 className='deck-name'>{name}</h2>
             <div className='collection-text-div'><h3>{collected} / {deckSize}</h3></div>
-            <div className='collection-bar-div'></div>
+            <div style={stylingObject.div}></div>
+            
           </div>
           
           <div className='deck-image-div'>
-            <img src="https://www.mtgpics.com/pics/art/one/010.jpg"></img>
+            <img src={image}></img>
           </div>
           
         </div>
