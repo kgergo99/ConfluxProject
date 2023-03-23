7fa1a55e48962bf479e38ebabf68a143fa85fc78
Karalyos Gergo <gergokaralyos@gmail.com>
2023-03-23 20:04:54 +0100

    Fixed dropdownbar. no more submit button

--- conflux-vite/src/modules/DropDownBar.jsx
+++ conflux-vite/src/modules/DropDownBar.jsx
@@ -7,19 +7,25 @@ function DropDownBar({ options, iconUrl, onSubmit, onChange }) {
     const handleReturn = (event) => {
         event.preventDefault();
         const selectedOption = event.target.elements.sortBy.value;
+        console.log("Sorting option changed: ", selectedOption)
+        onSubmit(selectedOption);
+    };
+
+    const handleOptionChange = (event) => {
+        const selectedOption = event.target.value;
+        console.log("Sorting option changed: ", selectedOption)
         onSubmit(selectedOption);
     };
   
     return (
         <form className="search-form" onSubmit={handleReturn}>
-            <select name="sortBy">
+            <select name="sortBy" onChange={handleOptionChange}>
                 {options.map((option) => (
                     <option key={option} value={option}>
                         {option}
                     </option>
                 ))}
             </select>
-            <button className='button-submit' type="submit" title="Sort" style={ { background: `no-repeat center url(${iconUrl})` } }></button>
         </form>
     )
 }
