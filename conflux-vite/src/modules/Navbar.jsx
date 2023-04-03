7bb1b74bfed1be2f3f9d89bb41f4676ed73fb8b0
Karalyos Gergo <gergokaralyos@gmail.com>
2023-04-03 14:33:02 +0200

    basic navbar added

--- conflux-vite/src/modules/Navbar.jsx
+++ conflux-vite/src/modules/Navbar.jsx
@@ -1,17 +1,74 @@
 import './navbar.css'
 import './modules.css'
-import { HambergerMenu, User } from 'iconsax-react';
+import { HambergerMenu, User, ArrowRight2, Arrow } from 'iconsax-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Navbar() {
+  const [navActiveState, setNavActiveState] = useState(false);
 
-    return (
-      <div className="nav-main">
-        <div className='navbar-item'><HambergerMenu href='#' className='svg-button menu' size="2.5rem" color="black" variant="Outline"/></div>     
-        <a href='#' className='logo-text navbar-item'>CONFLUX</a>
-        <div className='navbar-item'><User href='#' className='svg-button user' size="2.5rem" color="black" variant="Outline"/></div>
-      </div>
-    )
+  const handleNavMenuClick = () => {
+    setNavActiveState(!navActiveState);
   }
+
+  useEffect(()=>{
+    console.log(navActiveState)
+  },[navActiveState])
+
+  return (
+    <div className="nav-main">
+
+      <div className='navbar-item' onClick={handleNavMenuClick}>
+        <HambergerMenu className='svg-button menu' size="2.5rem" color="black" variant="Outline"/>
+        
+      </div>
+      
+
+      <Link to="/login"><p className='logo-text navbar-item'>CONFLUX</p></Link>
+
+      <div className='navbar-item'>
+        <User className='svg-button user' size="2.5rem" color="black" variant="Outline"/>
+      </div>
+      {navActiveState && 
+        <div className='nav-dropdown normal-container'>
+          <ul>
+            <li>
+              <Link to="/cards">
+                  <div className='menu-item-container'>
+                    <p>Collection</p>
+                    <div><ArrowRight2 className='svg-button menu' size="2rem" color="black" variant="Outline"/></div>
+                  </div>
+                </Link> 
+            </li>
+            <li>
+              <Link to="/stagingarea">
+                  <div className='menu-item-container'>
+                    <p>Add Cards</p>
+                    <div><ArrowRight2 className='svg-button menu' size="2rem" color="black" variant="Outline"/></div>
+                  </div>
+                </Link> 
+            </li>
+            <li>
+              <Link to="/decks">
+                  <div className='menu-item-container'>
+                    <p>Decks</p>
+                    <div><ArrowRight2 className='svg-button menu' size="2rem" color="black" variant="Outline"/></div>
+                  </div>
+                </Link> 
+            </li>
+            <li>
+              <Link to="/deckbuilder">
+                  <div className='menu-item-container'>
+                    <p>Build Deck</p>
+                    <div><ArrowRight2 className='svg-button menu' size="2rem" color="black" variant="Outline"/></div>
+                  </div>
+                </Link> 
+            </li>
+          </ul>
+        </div>}
+    </div>
+  )
+}
   
-  export default Navbar
+export default Navbar
   
