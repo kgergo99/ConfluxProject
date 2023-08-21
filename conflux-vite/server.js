4823afab006f6ff2a8f6e1ae34a019e091bd3a0e
Karalyos Gergo <gergokaralyos@gmail.com>
2023-08-21 19:54:52 +0200

    Reverted change, localhost allowed on cors

--- conflux-vite/server.js
+++ conflux-vite/server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://conflux-project.vercel.app"); //http://localhost:5173
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); //http://localhost:5173
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
